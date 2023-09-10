27cf930e058d5f375f216f6830ae9b41c625659c
Faisal <officialfaisal1112@gmail.com>
2023-09-10 19:53:07 +0600

    add more feature in home page

--- src/pages/Shared/Navbar/Navbar.tsx
+++ src/pages/Shared/Navbar/Navbar.tsx
@@ -102,8 +102,20 @@ const Navbar = () => {
         </button>
         <div className="dropdown dropdown-end">
           <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-            <div className="w-10 rounded-full">
-              <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+            <div className="w-6 rounded-full">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+                />
+              </svg>
             </div>
           </label>
           <ul
